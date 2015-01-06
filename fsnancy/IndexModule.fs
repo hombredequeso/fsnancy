3e1af11cd690e237ec4d1165e085ba00b09d17cd
Mark Cheeseman <mcheesemanau@gmail.com>
2015-01-07 00:43:38 +1100

    Reading from app settings
    Read a value from web.config appsettings and display on page

--- fsnancy/IndexModule.fs
+++ fsnancy/IndexModule.fs
@@ -1,8 +1,14 @@
 ﻿namespace fsnancy
 
 open Nancy
+open FSharp.Configuration
+
+type Settings = AppSettings<"web.config">
+type MyModel = {TestKey:string}
 
 type IndexModule() as x =
     inherit NancyModule()
-    do x.Get.["/"] <- fun _ -> box x.View.["index"]
+    let webConfigVal = Settings.TestKey
+    let m ={TestKey = webConfigVal}
+    do x.Get.["/"] <- fun _ -> box x.View.["index", m]
 
